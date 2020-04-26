094508732c29d76bdaf9437bb53eb2efb40b1201
Matthew Zhong <mzhong99@vt.edu>
2020-04-26 10:06:53 -0400

    Added predefined values for each port and pin

--- main.c
+++ main.c
@@ -5,6 +5,23 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/* Predefined values for LEDs */
+#define LED2_GREEN_PORT     GPIO_PORT_P2
+#define LED2_GREEN_PIN      GPIO_PIN1
+
+#define LED2_BLUE_PORT      GPIO_PORT_P2
+#define LED2_BLUE_PIN       GPIO_PIN2
+
+/* Predefined values for Buttons */
+#define S1_PORT             GPIO_PORT_P1
+#define S1_PIN              GPIO_PIN1
+
+#define S2_PORT             GPIO_PORT_P1
+#define S2_PIN              GPIO_PIN4
+
+#define PRESSED             0
+#define RELEASED            1
+
 int main(void)
 {
     /* Stop Watchdog */
