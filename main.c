ae3d58bd58b0ef4d7300d8851416cd843de91387
Matthew Zhong <mzhong99@vt.edu>
2020-04-26 09:34:35 -0400

    Created starter code

---
+++ main.c
@@ -0,0 +1,17 @@
+/* DriverLib Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    /* Stop Watchdog */
+    WDT_A_holdTimer();
+
+    while(1)
+    {
+        
+    }
+}
